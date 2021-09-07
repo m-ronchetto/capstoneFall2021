0c0c25d74071270ef186abb96074dd37e1f76959
m-ronchetto <54558548+m-ronchetto@users.noreply.github.com>
2021-09-07 10:47:08 -0500

    9/7 buttons

--- script.js
+++ script.js
@@ -63,25 +63,33 @@ document.addEventListener('DOMContentLoaded', (event) => {
       });
 
     //button 11: Self Destruct
-    let now = new Date().getTime();
-    let selfDestruct = now + 20000;
-    let x = setInterval(function() {
-        let distance = selfDestruct - now;
-        let seconds = Math.floor((distance % (1000 * 60)) / 1000);
-        document.getElementById("center-11-vertically").innerHTML = seconds + "s ";
-        if (distance < 0) {
-        clearInterval(x);
-        document.getElementById("center-11-vertically").innerHTML = "EXPIRED";
+    let x;
+    $("#button11").click(function(){
+        if (typeof x !== "undefined"){
+            clearInterval(x);
         }
-    }, 1000);
+        document.getElementById("center-11-vertically").innerHTML = "20s";
+        let seconds = 19;
+        x = setInterval(function() {
+            document.getElementById("center-11-vertically").innerHTML = seconds + "s ";
+            seconds = seconds-1;
+            if (seconds < 0) {
+            clearInterval(x);
+                document.getElementById("center-11-vertically").innerHTML = "EXPIRED";
+            }
+        }, 1000);
+    });
 
     //button 12: Generate more circles
     $("#button12").click(function(){
         let r = $("#circle").attr('data-height');
         let clone = $("#circle").clone();
-        clone.attr('data-height', '+ r + 20');
+        let randomX = Math.random()*240;
+        let randomY = (Math.random()- .2)*240;
+        clone.css({
+            position: "absolute", top: randomY, left: randomX
+        })
         clone.appendTo("#button12");
-        // $("#circle").clone().appendTo("#button12");
     });
 
     //button 13: Cat rotation
@@ -99,14 +107,65 @@ document.addEventListener('DOMContentLoaded', (event) => {
 
     //button 14: swap radial gradient
     $("#button14").click(function(){
-        if ($("#button14").css("background-image") === "radial-gradient(darkmagenta, aqua)"){
-            console.log("change1");
+        if ($("#button14").css("background-image") == "radial-gradient(rgb(139, 0, 139), rgb(0, 255, 255))"){
             $("#button14").css("background-image", "radial-gradient(aqua, darkmagenta)");
         }else{
-            console.log("change2");
             $("#button14").css("background-image", "radial-gradient(darkmagenta, aqua)");
         }
     });
+
+    //button 15: moving square
+    $("#button15").click(function(){
+        $("#hidden_square").toggle();
+        $("#hidden_square").css({
+            transform: 'scaleX(5)', transition: '3s'
+        });
+    });
+
+    //button 16: appear and disappear
+    $("#button16").click(function(){
+        $( "#button16wrap" ).toggle();
+        if ($("#button16").css("background-color") === "aqua"){
+            $("#button16").css("background-color", "#FFCE31");
+        } else{
+            console.log($("#button14").css("background-color"));
+            $("#button16").css("background-color", "aqua");
+        }
+    });
+
+    //button 17: icon rotate while background color changes
+    $("#button17").click(function(){
+        $("#button17").css({
+            // @keyframes colorChange {
+            //     from {background-color: burlywood;}
+            //     to {background-color: yellowgreen;}
+            // }
+            // #button17 {
+            //     animation-name: colorChange;
+            //     animation-duration: 4s;
+            //  }
+        });
+    });
+    
+    //button 18: generate random colored squares with opacity
+    $("#button18").click(function(){
+        let r = $("#square").attr('data-height');
+        let clone = $("#square").clone();
+        let randomX = Math.random()*240;
+        let randomY = (Math.random()- .2)*240;
+        let button_18_colors = ["#FF0000", "#FF8A00", "#FAFF00", "#00FF19", "#6100FF", "#DB00FF"];
+        let button_18_index = 0;
+        button_18_index = (button_18_index + 1)%button_18_colors.length;
+        clone.css({
+            position: "absolute", top: randomY, left: randomX, opacity: '.6', style: "color:" + button_18_colors[button_18_index] + ";"
+        })
+        clone.appendTo("#button18");
+    });
+
+    //return element from an array and increment index
+    function getElement(array, index){
+        
+    }
 
     //button 50: when clicked iterate over fonts
     let button_50_fonts = ["source-sans-pro", "helvetica", "garamond"];
